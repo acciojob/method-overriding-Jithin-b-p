9889961b177ebca627da56a1645c85cd720db0c4
unknown <jithinbp007@gmail.com>
2023-03-14 10:29:57 +0530

    method override

---
+++ src/main/java/com/driver/A.java
@@ -0,0 +1,16 @@
+package com.driver;
+
+class A{
+
+    public String meth(){
+
+        return "this is a string";
+
+    }
+}
+class B{
+    public String meth(){
+
+        return "Method is overridden in extendend class B";
+    }
+}
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,18 +1,5 @@
 package com.driver;
-class A{
 
-    public String meth(){
-
-        return "this is a string";
-
-    }
-}
-class B{
-    public String meth(){
-
-        return "Method is overridden in extendend class B";
-    }
-}
 public class Main {
     public static void main(String[] args) {
         B objB = new B();
