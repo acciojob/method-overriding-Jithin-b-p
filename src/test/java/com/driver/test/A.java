6ccfd04bd2ead9b42c2b983a21cc19567fb157b6
unknown <jithinbp007@gmail.com>
2023-03-14 10:34:58 +0530

    method override

---
+++ src/test/java/com/driver/test/A.java
@@ -0,0 +1,16 @@
+package com.driver.test;
+
+class A{
+
+    public String meth(){
+
+        return "this is a string";
+
+    }
+}
+class B{
+    public String meth(){
+
+        return "Method is overridden in extendend class B";
+    }
+}
